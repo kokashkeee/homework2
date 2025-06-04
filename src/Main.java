0ffc858678c94a3d455d02f31f65491e96ff0a9d
New <kravchenko.2455@gmail.com>
2025-06-04 16:48:28 +0700

    cycles

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,55 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("\nЗадание 1");
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf(i + " ");
+        }
+        System.out.println("\n\nЗадание 2");
+        for (int i = 10; i >= 1; i--) {
+            System.out.printf(i + " ");
+        }
+        System.out.println("\n\nЗадание 3");
+        for (int i = 0; i <= 17; i += 2) {
+            System.out.printf(i + " ");
+        }
+        System.out.println("\n\nЗадание 4");
+        for (int i = 10; i >= -10; i--) {
+            System.out.printf(i + " ");
+        }
+        System.out.println("\n\nЗадание 5");
+        for (int i = 1904; i <= 2096; i++) {
+            if (i % 4 == 0) {
+                System.out.printf(i + " ");
+            }
+        }
+        System.out.println("\n\nЗадание 6");
+        for (int i = 7; i <= 98; i += 7) {
+            System.out.printf(i + " ");
+        }
+        System.out.println("\n\nЗадание 7");
+        for (int i = 1; i <= 512; i *= 2) {
+            System.out.printf(i + " ");
+        }
+        System.out.println("\n\nЗадание 8");
+        int money = 29000;
+        int total = 0;
+        for (int i = 1; i <= 12; i++) {
+            total += money;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+        }
+        System.out.println("\nЗадание 9");
+        int money1 = 29000;
+        int total1 = 0;
+        for (int i = 1; i <= 12; i++) {
+            total1 *= 1.01;
+            total1 += money1;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total1 + " рублей");
+        }
+        System.out.println("\nЗадание 10");
+        int x;
+        for (int i = 1; i <= 10; i++) {
+            x = 2 * i;
+            System.out.println("2*" + i + "=" + x);
         }
     }
 }
