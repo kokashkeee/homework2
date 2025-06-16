f542d54c34d8b7bc80e80faeedb0527b06ba9c46
New <kravchenko.2455@gmail.com>
2025-06-16 23:14:30 +0700

    Strings 2

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,7 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //написал функцию ввода массива, сам, похвалите пж
-        //System.out.print("Введите количество элементов массива: ");
-        //Scanner scanner = new Scanner(System.in);
-        //int numOfEl = scanner.nextInt();
-        //int[] mass = new int[numOfEl];
-        //System.out.print("Введите значения элементов массива: ");
-        //for (int i = 0; i < numOfEl; i++) {
-        //    mass[i] = scanner.nextInt();
-        //}
         System.out.println("\nЗадание 1");
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите имя: ");
-        String firstName = scanner.next();
-        System.out.println("Введите фамилию: ");
-        String middleName = scanner.next();
-        System.out.println("Введите отчество: ");
-        String lastName = scanner.next();
-        String fullName = middleName + ' ' + firstName + ' ' + lastName;
-        System.out.println("Ф.И.О. сотрудника - " + fullName);
-
-        System.out.println("\nЗадание 2");
-        fullName = fullName.toUpperCase();
-        System.out.println("Ф.И.О. сотрудника - " + fullName);
-
-        System.out.println("\nЗадание 3");
-        fullName = fullName.replace('Ё', 'Е');
-        System.out.println("Ф.И.О. сотрудника - " + fullName);
     }
 }
